ed5510f5455dffab84e128d9128103271cfe9a00
Exil <exil@n0t3x1l.dev>
2024-10-18 15:23:50 +0000

    Revert "Switch to `play.ht` voice model"
    This reverts commit 524379511636044d4a8aa626376c0d25ec45196f.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import { Server } from 'socket.io';
-import * as PlayHT from 'playht';
 import http from 'http';
 import { getGroqChat } from './lib/groq.js'
 import { createClient, LiveTranscriptionEvents, }  from "@deepgram/sdk";
@@ -111,61 +110,32 @@ const setup = (ws) => {
           ws.close();
         }
         else {
-          PlayHT.init({
-            apiKey: process.env.PLAY_API_KEY,
-            userId: process.env.PLAY_USERID,
-        });
-          
+
           const responseText = await getGroqChat(caption, stack);
+          // log(`groq response: ${responseText}`)
+          const response = await deepgramClient.speak.request(
+            { text: responseText },
+            {
+              model: "aura-asteria-en",
+              encoding: "linear16",
+              container: "wav",
+            }
+          );
 
-          // const headers = await response.getHeaders();
-
-          const streamingOptions = {
-            // must use turbo for the best latency
-            voiceEngine: "PlayHT2.0-turbo",
-            //id: 'charlotte',
-            // this voice id can be one of our prebuilt voices or your own voice clone id, refer to the`listVoices()` method for a list of supported voices.
-            voiceId:
-               "s3://voice-cloning-zero-shot/d9ff78ba-d016-47f6-b0ef-dd630f59414e/female-cs/manifest.json",
-            // you can pass any value between 8000 and 48000, 24000 is default
-            sampleRate: 44100,
-            // the generated audio encoding, supports 'raw' | 'mp3' | 'wav' | 'ogg' | 'flac' | 'mulaw'
-            outputFormat: 'wav',
-            speed: 1,
-        };
-        const stream = await PlayHT.stream(responseText, streamingOptions);
+          console.log(response)
+      
+          const stream = await response.getStream();
+          const headers = await response.getHeaders();
           
           if (!stream) {
             throw new Error("Error generating audio stream");
           }
 
-          const streamToArrayBuffer = (stream) => {
-            return new Promise((resolve, reject) => {
-              const chunks = [];
-          
-              stream.on('data', (chunk) => {
-                chunks.push(chunk);
-              });
-          
-              stream.on('end', () => {
-                const buffer = Buffer.concat(chunks);
-                resolve(buffer.buffer.slice(buffer.byteOffset, buffer.byteOffset + buffer.byteLength));
-              });
-          
-              stream.on('error', (err) => {
-                reject(err);
-              });
-            });
-          };
-          
-          // Usage
-          const arrayBuffer = await streamToArrayBuffer(stream);
-
           if (stream) {
-            // const buffer = await getAudioBuffer(stream);
+            const buffer = await getAudioBuffer(stream);
             ws.emit('message',{
               'type': 'audio',
-              'output': arrayBuffer,
+              'output': Array.from(new Uint8Array(buffer)),
               'sid1': sid1,
               'sid2': sid2
             });
@@ -174,6 +144,9 @@ const setup = (ws) => {
             console.error("Error generating audio:", stream);
           }
         
+          if (headers) {
+            console.log("Headers:", headers);
+          }
 
 
         }
